be24342e5199eda80495d2c00c088971d9e487dc
tophe-w <128515611+tophe-w@users.noreply.github.com>
2024-09-06 16:36:59 +0200

    Ajout @Entity et lombok

--- src/main/java/com/example/pocJop/Models/Affluence.java
+++ src/main/java/com/example/pocJop/Models/Affluence.java
@@ -1,9 +1,17 @@
 package com.example.pocJop.Models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Affluence {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
