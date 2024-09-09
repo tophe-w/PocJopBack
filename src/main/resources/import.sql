33c72bdd43601c93ed51a80f2eff2ffc4c07566a
tophe-w <128515611+tophe-w@users.noreply.github.com>
2024-09-09 11:15:41 +0200

    Ajout fichier importSql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,27 @@
+INSERT INTO ligne (name, code)  VALUES ('A', 'A');
+
+INSERT INTO ligne (name,code)   VALUES ('B', 'B');
+
+INSERT INTO ligne (name, code)  VALUES ('C', 'C');
+
+INSERT INTO ligne (name,code)   VALUES ('D', 'D');
+
+INSERT INTO ligne (name,code)   VALUES ('E', 'E');
+
+INSERT INTO ligne (name,code)   VALUES ('H', 'H');
+
+INSERT INTO ligne (name, code)  VALUES ('J', 'J');
+
+INSERT INTO ligne (name,code)   VALUES ('K', 'K');
+
+INSERT INTO ligne (name,code)   VALUES ('L', 'L');
+
+INSERT INTO ligne (name,code)   VALUES ('N', 'N');
+
+INSERT INTO ligne (name,code)   VALUES ('P', 'P');
+
+INSERT INTO ligne (name,code)   VALUES ('R', 'R');
+
+INSERT INTO ligne (name,code)   VALUES ('U', 'U');
+
+INSERT INTO ligne (name,code)   VALUES ('T11', 'T11');
