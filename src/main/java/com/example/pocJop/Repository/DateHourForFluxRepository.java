5f27b6bd63fc07fafd758112496e790587059245
tophe-w <tophe1306@gmail.com>
2024-11-08 11:30:38 +0100

    Ajout repository

--- src/main/java/com/example/pocJop/Repository/DateHourForFluxRepository.java
+++ src/main/java/com/example/pocJop/Repository/DateHourForFluxRepository.java
@@ -1,8 +1,12 @@
 package com.example.pocJop.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.pocJop.Models.DateHourForFlux;
 
 public interface DateHourForFluxRepository extends JpaRepository<DateHourForFlux, Long> {
+
+    Optional<DateHourForFlux> findByAffluence_Id(Long affluenceId);
 
 }
